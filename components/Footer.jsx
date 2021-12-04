70a46a5682ca7b37a7ee17005e4c2ccdbf07cde8
imsikun <imsikun16@gmail.com>
2021-12-04 15:03:02 +0530

    added navbar and footer

---
+++ components/Footer.jsx
@@ -0,0 +1,16 @@
+import { Box } from '@chakra-ui/react';
+
+const Footer = () => {
+  return (
+    <Box
+      textAlign='center'
+      p='5'
+      color='gray.600'
+      borderTop='1px'
+      borderColor='gray.600'
+    >
+      2021 Real, Inc.
+    </Box>
+  );
+};
+export default Footer;
